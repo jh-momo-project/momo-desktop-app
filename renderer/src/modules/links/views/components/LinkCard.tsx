be794607d7c17d3329c8b8dac88c1941aae27e41
jihyun <hwiyu25@gmail.com>
2022-08-08 23:20:29 +0900

    :construction: LinkCard 일부 정보 반영

--- renderer/src/modules/links/views/components/LinkCard.tsx
+++ renderer/src/modules/links/views/components/LinkCard.tsx
@@ -1,5 +1,10 @@
+// @mui
 import { Card } from "@mui/material";
+// model
 import Link from "@modules/links/models/Link";
+// components
+import Text from "@components/Text";
+// utils
 import { openBrowser } from "@utils/linkUtils";
 
 interface ILinkCard {
@@ -8,8 +13,17 @@ interface ILinkCard {
 
 export default function LinkCard({ link }: ILinkCard) {
   return (
-    <Card sx={{ p: 2 }} onClick={() => openBrowser(link.url)}>
-      {link.title}
+    <Card
+      onClick={() => openBrowser(link.url)}
+      sx={{ width: "100%", padding: "12px 24px", cursor: "pointer", bgcolor: "common.white" }}
+    >
+      <Text size={10}>{link.categoryName}</Text>
+      <Text size={14} weight="bold">
+        {link.title}
+      </Text>
+      <Text size={11} color="grey.800">
+        {link.url}
+      </Text>
     </Card>
   );
 }
