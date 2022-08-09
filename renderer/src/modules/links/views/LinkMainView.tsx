4755d5aac6748872ba101d679b41e109e9c65c8a
jihyun <hwiyu25@gmail.com>
2022-08-09 23:03:50 +0900

    :sparkles: LinkCard Sorting 추가(drag n drop)

--- renderer/src/modules/links/views/LinkMainView.tsx
+++ renderer/src/modules/links/views/LinkMainView.tsx
@@ -11,10 +11,34 @@ import LinkDirectoryCard from "@modules/links/views/components/LinkDirectoryCard
 import LinkMainHeader from "@modules/links/views/components/LinkMainHeader";
 import LinkCard from "@modules/links/views/components/LinkCard";
 
+// dnd-kit
+import {
+  DndContext,
+  useDroppable,
+  useSensors,
+  useSensor,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+} from "@dnd-kit/core";
+import { SortableContext, rectSortingStrategy } from "@dnd-kit/sortable";
+
 export default function LinkMainView() {
   const { data: categories } = useGetCategories();
   const [currentId, setCurrentDirectory] = useState(categories[0].id);
   const { isLoading, data: links } = useGetLinks({ categoryId: currentId });
+
+  // dnd-kit
+  const { isOver, setNodeRef } = useDroppable({ id: "linksDroppable" });
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+    useSensor(TouchSensor)
+  );
+
   return (
     <>
       <LinkMainHeader />
@@ -29,14 +53,20 @@ export default function LinkMainView() {
             />
           ))}
         </DirectoryList>
-        <Box sx={{ width: "100%" }}>
-          <LinkList container spacing={2}>
-            {!isLoading &&
-              links.map((link) => (
-                <Grid item key={link.id} xs={12} sm={6} md={4} lg={3} xl={2} sx={{ flexGrow: 0 }}>
-                  <LinkCard link={link} />
-                </Grid>
-              ))}
+        <Box sx={{ width: "100%", height: "100%" }}>
+          <LinkList container spacing={2} ref={setNodeRef}>
+            <DndContext onDragEnd={(e) => console.log(e)} sensors={sensors} collisionDetection={closestCenter}>
+              {links && (
+                <SortableContext items={links} strategy={rectSortingStrategy}>
+                  {!isLoading &&
+                    links.map((link) => (
+                      <Grid item key={link.id} xs={12} sm={6} md={4} lg={3} xl={2} sx={{ flexGrow: 0 }}>
+                        <LinkCard link={link} />
+                      </Grid>
+                    ))}
+                </SortableContext>
+              )}
+            </DndContext>
           </LinkList>
         </Box>
       </ViewContainer>
--- renderer/src/modules/links/views/components/LinkCard.tsx
+++ renderer/src/modules/links/views/components/LinkCard.tsx
@@ -7,23 +7,35 @@ import Text from "@components/Text";
 // utils
 import { openBrowser } from "@utils/linkUtils";
 
+// dnd-kit
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 interface ILinkCard {
   link: Link;
 }
 
 export default function LinkCard({ link }: ILinkCard) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: link.id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <Card
-      onClick={() => openBrowser(link.url)}
-      sx={{ width: "100%", padding: "12px 24px", cursor: "pointer", bgcolor: "common.white" }}
-    >
-      <Text size={10}>{link.categoryName}</Text>
-      <Text size={14} weight="bold">
-        {link.title}
-      </Text>
-      <Text size={11} color="grey.800">
-        {link.url}
-      </Text>
-    </Card>
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      <Card
+        onClick={() => openBrowser(link.url)}
+        sx={{ width: "100%", padding: "12px 24px", cursor: "pointer", bgcolor: "common.white" }}
+      >
+        <Text size={10}>{link.categoryName}</Text>
+        <Text size={14} weight="bold">
+          {link.title}
+        </Text>
+        <Text size={11} color="grey.800">
+          {link.url}
+        </Text>
+      </Card>
+    </div>
   );
 }
